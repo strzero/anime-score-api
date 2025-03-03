6f440ce3b102fe372eb01b6978b600d41a2eea9f
StellateZero <stellatezero@outlook.com>
2025-03-03 19:50:54 +0800

    update

--- api/main.py
+++ api/main.py
@@ -20,7 +20,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "mysql+aiomysql://root:so6666@localhost:3306/anime-score"
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 logging.basicConfig(
