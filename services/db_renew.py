982bef13064f7a10b27e249f8fa7cde1811e809a
StellateZero <stellatezero@outlook.com>
2025-03-20 16:51:42 +0800

    feat: 过期数据定时更新

--- services/db_renew.py
+++ services/db_renew.py
@@ -1,3 +1,4 @@
+import asyncio
 from datetime import datetime
 from typing import List
 from pydantic import BaseModel
@@ -13,22 +14,22 @@ class QueryRequest(BaseModel):
     bangumi_id: int
 
 async def renew_data():
-    logger.info("定时检查数据是否过期...")
-    now = datetime.utcnow()
-    
-    expired_scores = await Score.filter(expire_time__lt=now).all()
-    
-    if not expired_scores:
-        logger.info("无过期数据")
-        return
-    query_list = [QueryRequest(bangumi_id=score.bangumi_id) for score in expired_scores]
-    logger.info("开始更新数据")
-    
-    async with in_transaction():
-        await Score.filter(Q(expire_time__lt=now)).delete()
-    
-    result = await query(query_list)
-    logger.info("数据更新执行完毕")
+    while(True):
+        logger.info("定时检查数据是否过期...")
+        now = datetime.utcnow()
+        
+        expired_scores = await Score.filter(expire_time__lt=now).all()
+        
+        if not expired_scores:
+            logger.info("无过期数据")
+            return
+        query_list = [QueryRequest(bangumi_id=score.bangumi_id) for score in expired_scores]
+        logger.info("开始更新数据")
+        
+        async with in_transaction():
+            await Score.filter(Q(expire_time__lt=now)).delete()
+        
+        result = await query(query_list)
+        logger.info("数据更新执行完毕")
 
-async def renew_data_crontab():
-    aiocron.crontab("*/30 * * * *", func=renew_data)
+        asyncio.sleep(3600)
