e41fdeebafd572e374286be94930e74f623ad2e7
StellateZero <stellatezero@outlook.com>
2025-03-27 21:23:33 +0800

    fix: 尝试修复db超时

--- maintain/maintain_db.py
+++ maintain/maintain_db.py
@@ -1,4 +1,6 @@
 import asyncio
+import os
+from datetime import datetime
 from maintain.download_bangumi_data import download_bangumi_data
 from maintain.update_bangumi_data import update_bangumi_data
 from utils.client import client
@@ -6,25 +8,36 @@ from utils.logger import logger
 from models.db_model import Info
 from tortoise.exceptions import DBConnectionError
 
+data_dir = os.path.join("maintain", "data")
+version_file = os.path.join(data_dir, "db_version.txt")
 
 async def get_local_version_time():
     """ 获取本地存储的数据库更新时间 """
     try:
-        # 直接查询 Info 表，获取单一记录的 'value' 字段
         result = await Info.filter(variable='time').first()
         return result.value if result else None
     except DBConnectionError:
         logger.warning("数据库连接丢失，尝试重新连接...")
         return None
 
+def get_file_version_time():
+    """ 获取本地存储的数据库版本文件时间 """
+    if os.path.exists(version_file):
+        return datetime.fromtimestamp(os.path.getmtime(version_file)).isoformat()
+    return None
 
 async def update_local_version_time(new_time):
     """ 更新本地存储的数据库更新时间 """
     try:
-        await Info.filter(variable='time').update(value=new_time)
+        await Info.update_or_create(defaults={'value': new_time}, variable='time')
     except DBConnectionError:
         logger.error("更新数据库版本时间失败，可能是连接丢失")
 
+def update_file_version_time(new_time):
+    """ 更新本地数据库版本文件 """
+    os.makedirs(data_dir, exist_ok=True)
+    with open(version_file, "w") as f:
+        f.write(new_time)
 
 async def maintain_db():
     while True:
@@ -39,15 +52,20 @@ async def maintain_db():
                 await asyncio.sleep(43200)
                 continue
 
-            # 获取本地数据库版本时间
-            local_version_time = await get_local_version_time()
+            # 先更新数据库时间，避免长时间连接丢失问题
+            await update_local_version_time(web_version_time)
 
-            if web_version_time != local_version_time:
+            # 获取本地数据库版本时间和文件时间
+            local_version_time = await get_local_version_time()
+            file_version_time = get_file_version_time()
+
+            if web_version_time != local_version_time or web_version_time != file_version_time:
                 logger.info("Bangumi数据库有新版本，开始更新...")
                 if download_bangumi_data():
                     if update_bangumi_data():
                         logger.info("Bangumi数据库更新成功！")
                         await update_local_version_time(web_version_time)
+                        update_file_version_time(web_version_time)
                     else:
                         logger.error("Bangumi数据库更新失败！")
                 else:
